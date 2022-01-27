d4f822b265115ed4d10fdc1a873e586d0fd6628b
GatorBash <95302944+GatorBash@users.noreply.github.com>
2022-01-27 16:15:56 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-echo "installing needed software"
-echo "be ready to put in your creds"
+while true 
+do
+  echo "installing needed software"
+  echo "be ready to put in your creds"
 
-sudo apt install dfu-util hackrf -y
-wait
-echo "plug in the hackrf while holding down the DFU button"
+  sudo apt install dfu-util hackrf -y
+  echo ==========================
+  wait
+  echo "plug in the hackrf while holding down the DFU button"
+  echo "Its the second button from the right"
+  sleep 5
+  echo "did you hold the button down when you plugged it in? y/n"
+  read a
+  if [ "$a" = y ]
+  then
+    dfu-util --device 1fc9:000c --download hackrf_usb.dfu --reset
+    wait
+    hackrf_spiflash -w portapack-h1_h2-mayhem.bin
+    wait
+    
